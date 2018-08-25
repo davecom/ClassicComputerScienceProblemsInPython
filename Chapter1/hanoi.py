f8f6210675024ffcc6a200b8126decb89224f592
Dave <snpukwana@gmail.com>
2018-08-25 10:27:52 -0400

    little fix in hanoi

--- Chapter1/hanoi.py
+++ Chapter1/hanoi.py
@@ -19,7 +19,7 @@ T = TypeVar('T')
 
 class Stack(Generic[T]):
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._container: List[T] = []
 
     def push(self, item: T) -> None:
@@ -28,7 +28,7 @@ class Stack(Generic[T]):
     def pop(self) -> T:
         return self._container.pop()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return repr(self._container)
 
 
