8c3102fd7866a756eb1be43a67e49fb52c12eb07
Dave <snpukwana@gmail.com>
2018-08-28 20:27:54 -0400

    Utility functions for chapter 7

---
+++ Chapter7/util.py
@@ -0,0 +1,30 @@
+# util.py
+# From Classic Computer Science Problems in Python Chapter 7
+# Copyright 2018 David Kopec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import List
+from math import exp
+
+
+def dot(xs: List[float], ys: List[float]) -> float:
+    return sum(x * y for x, y in zip(xs, ys))
+
+
+def sigmoid(x: float) -> float:
+    return 1.0 / (1.0 + exp(x))
+
+
+def derivative_sigmoid(x: float) -> float:
+    sig = sigmoid(x)
+    return sig * (1 - sig)
