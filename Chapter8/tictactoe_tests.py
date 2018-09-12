499a09fbe2976e71f7fc238e1345248eb2e5937c
Dave <snpukwana@gmail.com>
2018-09-11 23:39:15 -0400

    Added missing import

--- Chapter8/tictactoe_tests.py
+++ Chapter8/tictactoe_tests.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
+from typing import List
 from minimax import find_best_move
 from tictactoe import TTTPiece, TTTBoard
 from board import Move
