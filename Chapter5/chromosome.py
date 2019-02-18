e113648dcd36c37dee6f6190b58a7504a0a85e4b
Steve Nicholson <ssteve@mac.com>
2019-02-18 14:39:07 -0800

    Small typo in comment

--- Chapter5/chromosome.py
+++ Chapter5/chromosome.py
@@ -20,7 +20,7 @@ from abc import ABC, abstractmethod
 T = TypeVar('T', bound='Chromosome') # for returning self
 
 
-# Base class for all chromosomes; all methods must be o overridden
+# Base class for all chromosomes; all methods must be overridden
 class Chromosome(ABC):
     @abstractmethod
     def fitness(self) -> float:
